dd11a290fc94e0ff8e5b7985e01a5ec6e81691a2
baishanziyu <goodluckiyu@gmail.com>
2020-08-27 10:14:39 +0800

    20200827config端口修改

--- pixellot_web/assets/js/config.js
+++ pixellot_web/assets/js/config.js
@@ -1,8 +1,8 @@
 var pixellot = axios.create({
-	  baseURL: 'http://52.82.60.3:5000/',
+	  baseURL: 'http://52.82.60.3:81/',
 	  headers: {'Authorization': 'pixellot ' + localStorage.getItem('token')}		
 });
 var pixellot_login = axios.create({
-	  baseURL: 'http://52.82.60.3:5000/'
+	  baseURL: 'http://52.82.60.3:81/'
 });
 
